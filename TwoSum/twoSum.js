d7c73472265c8aa4d773ca04ad8ccb3b495dddd5
michaelslaton <michaelaaronphoto@gmail.com>
2022-05-10 12:24:46 -0700

    Initial commit.

---
+++ TwoSum/twoSum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i === j) continue;
+      if (nums[i] + nums[j] === target) {
+        result = [j, i];
+      }
+    }
+  }
+
+  return result;
+};
