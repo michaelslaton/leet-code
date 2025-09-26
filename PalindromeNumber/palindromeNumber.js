2ee00911b45ab3ad77828222a25f954d2015ef26
RenShadecrest <michaelaaronphoto@gmail.com>
2025-09-25 18:47:07 -0700

    Redo of palindrome number - my code is longer but I feel more readable and includes an edge case

--- PalindromeNumber/palindromeNumber.js
+++ PalindromeNumber/palindromeNumber.js
@@ -1,10 +1,9 @@
  var isPalindrome = function(x) {
-
   if(Number(Array.from(String(x), Number).reverse().join('')) === x) return true;
-
   return false;
 };
 
 console.log(isPalindrome(1221)); // True
 console.log(isPalindrome(939)); // True
+console.log(isPalindrome(-939)); // false
 console.log(isPalindrome(12345)); // False
---
+++ PalindromeNumber/palindromeNumberRedo.js
@@ -0,0 +1,11 @@
+const isPalindrome = (x) => {
+    if (x < 0) return false;
+    const reversedInput = Number(x.toString().split('').reverse().join(''));
+    if (reversedInput === x)  return true;
+    return false;
+};
+
+console.log(isPalindrome(1221)); // True
+console.log(isPalindrome(939)); // True
+console.log(isPalindrome(-939)); // false
+console.log(isPalindrome(12345)); // False
