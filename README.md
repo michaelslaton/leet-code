2cb575ed7053ddcaa45f41e717c85ce32fa70deb
RenShadecrest <michaelaaronphoto@gmail.com>
2025-09-28 12:24:16 -0700

    Roman To Integer Redo - I don't think I solved this before, I think I over complicated it in my head. This time I was able to solve it, and I actually think I did a better job than the version I looked up before.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # leet-code
 A repository of my leetcode solutions.
+
+# Update: 9/2025
+I'm going through these problems again and giving another pass at them since I've continued to work on my skills since them, I also intend to continue going through them one by one.
---
+++ RomanToInteger/romanToIntegerRedo.js
@@ -0,0 +1,19 @@
+const romanToInt = (x) => {
+  const numerals = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  let results = 0;
+
+  for(let i=0;i<x.length;i++){
+    let current = numerals[x[i]];
+    let next = numerals[x[i + 1]]
+    if(current < next) results -= current;
+    else results += current;
+  };
+
+  return results;
+};
+
+console.log(romanToInt("III")); // 3
+console.log(romanToInt("IV")); // 4
+console.log(romanToInt("LVIII")); // 58
+console.log(romanToInt("MCMXCIV")); // 1994
+console.log(romanToInt("XIV")) // 14
